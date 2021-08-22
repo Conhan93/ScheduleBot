c107899fcd930611c61a8d95a37db71cb0cf2b11
Conny Hansson <conhan@hotmail.se>
2021-08-22 19:19:38 +0200

    debug messages to task.loop

--- start.py
+++ start.py
@@ -10,14 +10,20 @@ scheduleclient = ScheduleBot()
 
 @tasks.loop(minutes=2)
 async def update_schedule_monday():
-        if datetime.datetime.now().hour == 19:
-            print("clk is")
-            if datetime.datetime.today().weekday() == 6:
-                print("it's sunday")
-                channel = scheduleclient.get_channel(877212291056169050)
-                await channel.send(scheduleclient.get_schedule_current("iot20"))
-        #sleep(60)
-        #await asyncio.sleep(60)
+    print("looping")
+    if datetime.datetime.now().hour == 19:
+        print("clk is")
+        if datetime.datetime.today().weekday() == 6:
+            print("it's sunday")
+            channel = scheduleclient.get_channel(877212291056169050)
+            await channel.send(scheduleclient.get_schedule_current("iot20"))
+        else:
+            print("wrong day, day is : " + datetime.datetime.today().weekday())
+    else:
+        print("wrong hour, hour is :" + datetime.datetime.now().hour)
+    #sleep(60)
+    #await asyncio.sleep(60)
+    print("loop done")
             
 update_schedule_monday.start()
 #timer = asyncio.get_event_loop().create_task(update_schedule_monday())
